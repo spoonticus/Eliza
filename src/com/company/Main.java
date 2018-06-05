7188f0b1bcbf8e6216e749ed4348222b1e0d6856
Doe <olivia.r.doe@gmail.com>
2018-06-05 11:02:29 -0400

    06MAY2018

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,55 +15,66 @@ public class Main {
 
         System.out.println("Good day, my name is Eliza. Tell me what is on your mind.");
         System.out.println("Press q at anytime to exist session.");
-        String comment = input.readLine();
-
-        String [] replacement = comment.split("\\s+");
-            for (int i=0; i<replacement.length; i++){
-                if (replacement[i].equalsIgnoreCase("I")){
-                    replacement[i] = "you";
-                }
-                if (replacement[i].equalsIgnoreCase("my")){
-                    replacement[i] = "your";
-                }
-                if (replacement[i].equalsIgnoreCase("me")){
-                    replacement[i] = "you";
-                }
-                if (replacement[i].equalsIgnoreCase("am")){
-                    replacement[i] = "are";
-                }
-
-                if (random.nextBoolean()){
-                    replyHdg();
-                } else {
-                    replyQualfr();
-                }
-
-
-
+        while (true) {
+            String comment = input.readLine();
+            if(comment.equalsIgnoreCase("q")){
+                break;
             }
 
-        //ArrayList<String> comment = new ArrayList<String>();
+            String[] replacement = comment.split("\\s+");
+            for (int i = 0; i < replacement.length; i++) {
+                if (replacement[i].equalsIgnoreCase("I")) {
+                    replacement[i] = "you";
+                } else if (replacement[i].equalsIgnoreCase("my")) {
+                    replacement[i] = "your";
+                } else if (replacement[i].equalsIgnoreCase("me")) {
+                    replacement[i] = "you";
+                } else if (replacement[i].equalsIgnoreCase("am")) {
+                    replacement[i] = "are";
+                } else {
+                    replacement[i] = replacement[i];
+                }
+            }
+            String replcmnt = new String();
+            for (int i = 0; i < replacement.length; i++) {
+                replcmnt += replacement[i] + " ";
+            }
 
+            System.out.println("So if I heard you right. " + replcmnt);
+
+            String respnse = new String();
+            if (random.nextBoolean()) {
+                respnse = replyHdg();
+            } else {
+                respnse = replyQualfr();
+            }
+            System.out.println(respnse);
         }
 
-    private static void replyQualfr() {
-        Random random = new Random();
-        ArrayList<String> qualifiers =new ArrayList<String>();
-        qualifiers.add("Why do you say that");
-        qualifiers.add("You seem to think that");
-        qualifiers.add("So, you are concerned that");
-        String replyQulfr = qualifiers.get(random.nextInt(qualifiers.size()));
+    }//close method main
+        //ArrayList<String> comment = new ArrayList<String>();
+
+
+        private static String replyQualfr() {
+            Random random = new Random();
+            ArrayList<String> qualifiers = new ArrayList<String>();
+            qualifiers.add("Why do you say that");
+            qualifiers.add("You seem to think that");
+            qualifiers.add("So, you are concerned that");
+            String replyQulfr = qualifiers.get(random.nextInt(qualifiers.size()));
+            return replyQulfr;
+        }
+
+        private static String replyHdg() {
+            Random random = new Random();
+            ArrayList<String> hedges = new ArrayList<String>();
+            hedges.add("Please tell me more");
+            hedges.add("Many of my patients tell me the same thing");
+            hedges.add("It is getting late, maybe we had better quit");
+            String replyHdg = hedges.get(random.nextInt(hedges.size()));
+            return replyHdg;
+        }
+
+
     }
-
-    private static void replyHdg() {
-        Random random = new Random();
-        ArrayList<String> hedges =new ArrayList<String>();
-        hedges.add("Please tell me more");
-        hedges.add("Many of my patients tell me the same thing");
-        hedges.add("It is getting late, maybe we had better quit");
-        String replyHdg = hedges.get(random.nextInt(hedges.size()));
-    }
-
-
-}
 
